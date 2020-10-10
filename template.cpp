7c4dec0e47e046f6a5f89a66fbc7ef44819833a8
PRIYANSHU GOEL <goelpriyanshu99@gmail.com>
2020-10-10 15:51:38 +0530

    CPP Template

---
+++ template.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define vi vector<int>
+#define pii pair<int, int>
+#define all(vec) vec.begin(), vec.end()
+#define ischar(ch) (ch >= 'a' && ch <= 'z')
+#define isChar(ch) (ch >= 'A' && ch <= 'Z')
+#define fill(vec, x) memset(vec, x, sizeof(vec))
+#define quickio                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+// Input macros
+#define si(n) scanf("%d", &n)
+#define sc(n) scanf("%c", &n)
+#define sl(n) scanf("%lld", &n)
+#define sf(n) scanf("%lf", &n)
+
+typedef long long int ll;
+typedef unsigned long long ull;
+const long double EPS = 1e-9;
+const long double PI = 3.14159265358979;
+inline int gcd(int a, int b) { return (b ? gcd(b, a % b) : a); }
+inline int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+const ll mod = 1e9 + 7;
+const ll maxn = 5000;
+
+int main()
+{
+    quickio;
+
+    //freopen("input.in", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t = 1;
+    //cin>>t;
+    
+    while (t--)
+    {
+    }
+
+    return 0;
+}
+
+//goel28priyanshu
