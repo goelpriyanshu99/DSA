f815b76fba182e79de75c2526d2e2af089f99a53
PRIYANSHU GOEL <goelpriyanshu99@gmail.com>
2021-12-06 19:57:56 +0530

    Create 06Dec_MinimumCostToMoveChipsToTheSamePosition.cpp

---
+++ LeetCode/Dec 2021/06Dec_MinimumCostToMoveChipsToTheSamePosition.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int x = 0, y = 0;
+        for(int &it: position) {
+            if(it%2 == 0) x++;
+            else y++;
+        }
+        return min(x,y);
+    }
+};
